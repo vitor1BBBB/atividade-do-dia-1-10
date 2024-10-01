a63d5b583056d7132899cd628abe710a3509fd48
vitor1B <147104338+vitor1BBBB@users.noreply.github.com>
2024-10-01 14:56:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é index.html?',
+    'Criada pelo britânico Tim Berners-Lee, o acrônimo HTML significa HiperText Markup Language, traduzindo ao português: Linguagem de Marcação de Hipertexto.  O HTML é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website. Portanto, ele serve para dar significado e organizar as informações de uma página na web. Sem isso, o navegador não saberia exibir textos como elementos ou carregar imagens e outros conteúdos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve o style CSS?',
+    'CSS é uma linguagem baseada em regras. — Você define regras especificando grupos de estilo que devem ser aplicados para elementos particulares ou grupos de elementos na sua página web. Por exemplo, "Quero que o título principal, na minha página, seja mostrado como um texto grande e de cor vermelha.".'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa noite em Inglês?',
+    'Boa noite em ingles é Goodnight.'
+)
+criaCartao(
+    'Lingua Espanhol',
+    'Como pedir licença em Espanhol?',
+    'Pedir licença em Espanhol é "con permiso"'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é taxa Selic?',
+    'A Selic é a taxa de juros média praticada nas operações compromissadas com títulos públicos federais com prazo de um dia útil. O BC realiza operações no mercado de títulos públicos para que a taxa Selic efetiva esteja em linha com a meta da taxa Selic, que é definida pelo Comitê de Política Monetária (Copom) do BC.'
+)
